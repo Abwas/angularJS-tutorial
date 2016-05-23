2eacd779a0a674b0220c963f7e6bbb7e0b9388f5
max33nau <mtj29@nau.edu>
2016-05-23 02:32:40 -0700

    added our first data-binding expression

--- app.js
+++ app.js
@@ -2,7 +2,16 @@
 // That module takes the name of your app and takes a array of dependencies.
 var app = angular.module('myApp', []);
 
-//.controllers take a name and a function
-app.controller('mainCtrl', function(){
+/* What is dependency injection? https://www.youtube.com/watch?v=ejBkOjEG6F0 time: 33:31 */
+// Giving a function a object, rather than creating a object inside a function we pass it
+// into the function
 
+// $scope is big complex object that comes from dependency injection.
+// All angularJS services start with a $ sign.
+// We can now add on to the $scope object
+
+//.controllers take a name and a function
+app.controller('mainCtrl', function($scope){
+  // Variables stored on the $scope object are known as the model
+  $scope.name = 'Max';
 });
