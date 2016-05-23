44072567dcb1f01d74df32bc2a663a8886fadbe7
max33nau <mtj29@nau.edu>
2016-05-23 02:17:00 -0700

    added a main controller

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // In angular.js.min there is global angular object and inside that object is a module.
 // That module takes the name of your app and takes a array of dependencies.
 var app = angular.module('myApp', []);
+
+//.controllers take a name and a function
+app.controller('mainCtrl', function(){
+
+});
