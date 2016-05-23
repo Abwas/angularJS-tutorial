676f193298a73461360e9e24b0ee971340661cfd
max33nau <mtj29@nau.edu>
2016-05-23 02:10:35 -0700

    created our global namespace angular module called myApp

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-// Angular App Will GO HERE 
+// In angular.js.min there is global angular object and inside that object is a module.
+// That module takes the name of your app and takes a array of dependencies.
+var app = angular.module('myApp', []);
