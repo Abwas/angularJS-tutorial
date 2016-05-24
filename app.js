071381d3a4cfa99af8284bf31a08178501f38b63
max33nau <mtj29@nau.edu>
2016-05-23 21:36:56 -0700

    adds a description

--- app.js
+++ app.js
@@ -2,5 +2,11 @@
 var app = angular.module('myApp', []);
 
 app.controller('ngClassCtrl', function($scope){
-  $scope.name = 'Max';
+  $scope.questions = {};
 });
+
+// Description
+/* ng-class directive allows you to set CSS
+classes to specific HTML elements by data binding
+an expression which will represent all the
+classes to be added. */ 
